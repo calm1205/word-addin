46ae6c21ea5efeb3ffea17e65e0772ff5ed3e97c
calm1205 <naginani072@gmail.com>
2024-02-02 17:39:28 +0900

    insert wef

--- script/show_wef.sh
+++ script/show_wef.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-LOCAL_MANIFEST=./manifest.xml
 TARGET_MANIFEST_DIR=/Users/$(id -un)/Library/Containers/com.microsoft.Word/Data/Documents/wef
-TARGET_FILE_NAME=private.manifest.xml
 
-cp -f ${LOCAL_MANIFEST} ${TARGET_MANIFEST_DIR}/${TARGET_FILE_NAME}
 ls -l ${TARGET_MANIFEST_DIR}
