286ef6b0c08edfc65d701b7a95b00e993f80ad47
calm1205 <naginani072@gmail.com>
2024-02-02 17:15:32 +0900

    npm install office-addin-manifest

--- script/set_manifest.sh
+++ script/set_manifest.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-cp -f ./manifest-localhost.xml /Users/$(id -un)/Library/Containers/com.microsoft.Word/Data/Documents/wef/.
+LOCAL_MANIFEST=./manifest.xml
+TARGET_MANIFEST_DIR=/Users/$(id -un)/Library/Containers/com.microsoft.Word/Data/Documents/wef
+TARGET_FILE_NAME=private.manifest.xml
+
+cp -f ${LOCAL_MANIFEST} ${TARGET_MANIFEST_DIR}/${TARGET_FILE_NAME}
+ls -l ${TARGET_MANIFEST_DIR}
