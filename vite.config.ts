59cc6cb64f24782904d798a5edb77b0b77f84806
calm1205 <naginani072@gmail.com>
2024-02-05 10:09:25 +0900

    rm @vitejs/plugin-basic-ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 9999,
+    https: {
+      key: fs.readFileSync("./src/ssl/localhost.key"),
+      cert: fs.readFileSync("./src/ssl/localhost.crt"),
+    },
   },
-  plugins: [
-    vue(),
-    basicSsl({
-      name: "localhost",
-      domains: ["localhost"],
-      certDir: "./certs",
-    }),
-  ],
+  plugins: [vue()],
 });
