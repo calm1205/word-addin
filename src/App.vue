ff918f223677596ffb3e0f03cf2b2b9c6d643441
calm1205 <naginani072@gmail.com>
2024-02-05 13:22:20 +0900

    rm dont need style

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  window.Office.onReady();
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> </a>
-    <a href="https://vuejs.org/" target="_blank"> </a>
-    <img src="./assets/icon-32.png" alt="" />
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>hello world</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
