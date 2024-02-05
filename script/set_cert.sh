c97a43afd623ca3d66b4af36c76636c79f21a194
calm1205 <naginani072@gmail.com>
2024-02-05 10:10:15 +0900

    ignroe crt key

---
+++ script/set_cert.sh
@@ -0,0 +1,4 @@
+
+npx office-addin-dev-certs install
+cp ~/.office-addin-dev-certs/localhost.key ./src/ssl/localhost.key
+cp ~/.office-addin-dev-certs/localhost.crt ./src/ssl/localhost.crt
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,7 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 
+Office.onReady((info) => {
+  console.log(info);
+});
 createApp(App).mount("#app");
