5666cbb6e2e320dd76d3e0d02ff54d5e4d24f85b
calm1205 <naginani072@gmail.com>
2024-03-13 18:17:32 +0900

    imageをassetsに移動

--- src/views/Icon16.vue
+++ src/views/Icon16.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+import icon16 from "/assets/icon-16.png";
+</script>
+
 <template>
-  <img src="@/assets/icon-16.png" alt="icon 16px" />
+  <img :src="icon16" alt="icon 16px" />
 </template>
